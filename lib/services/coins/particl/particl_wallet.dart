095a3dc0a3d5caee94ebc6da79603560113648f7
sneurlax <sneurlax@gmail.com>
2022-12-01 20:58:02 -0600

    remove duplicate network definition
    fulfilled by bitcoindart now

--- lib/services/coins/particl/particl_wallet.dart
+++ lib/services/coins/particl/particl_wallet.dart
@@ -3857,12 +3857,3 @@ class ParticlWallet extends CoinServiceAPI {
     }
   }
 }
-
-// Particl Network
-final particl = NetworkType(
-    messagePrefix: '\x18Bitcoin Signed Message:\n',
-    bech32: 'pw',
-    bip32: Bip32Type(public: 0x696e82d1, private: 0x8f1daeb8),
-    pubKeyHash: 0x38,
-    scriptHash: 0x3c,
-    wif: 0x6c);
