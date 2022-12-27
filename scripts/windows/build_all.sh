ad5524d623997011c5d30b4d87df28106dfe08a0
sneurlax <sneurlax@gmail.com>
2022-12-27 12:09:06 -0600

    consolidate built libraries into top-level stack_wallet folder

--- scripts/windows/build_all.sh
+++ scripts/windows/build_all.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 mkdir -p build
-(cd ../../crypto_plugins/flutter_libepiccash/scripts/windows && ./build_all.sh )  &
-(cd ../../crypto_plugins/flutter_liblelantus/scripts/windows && ./build_all.sh ) &
-(cd ../../crypto_plugins/flutter_libmonero/scripts/windows && ./build_all.sh) &
+(cd ../../crypto_plugins/flutter_libepiccash/scripts/windows && ./build_all.sh && cp build/libepic_cash_wallet.dll ../../../../)  &
+(cd ../../crypto_plugins/flutter_liblelantus/scripts/windows && ./build_all.sh && cp build/libmobileliblelantus.dll ../../../../) &
+(cd ../../crypto_plugins/flutter_libmonero/scripts/windows && ./build_all.sh && cp build/libcw_monero.dll ../../../../ && cp build/libcw_wownero.dll ../../../../) &
 
 wait
 echo "Done building"
