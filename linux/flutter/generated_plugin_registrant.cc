7458881aabc6c199480a1812616421563a9ef84d
sneurlax <sneurlax@gmail.com>
2022-12-30 11:20:34 -0600

    ref update: cw_shared_external -> wow_cw_shared_external

--- linux/flutter/generated_plugin_registrant.cc
+++ linux/flutter/generated_plugin_registrant.cc
@@ -9,6 +9,7 @@
 #include <devicelocale/devicelocale_plugin.h>
 #include <flutter_libepiccash/flutter_libepiccash_plugin.h>
 #include <flutter_libmonero/flutter_libmonero_plugin.h>
+#include <flutter_libwownero/flutter_libmonero_plugin.h>
 #include <flutter_secure_storage_linux/flutter_secure_storage_linux_plugin.h>
 #include <isar_flutter_libs/isar_flutter_libs_plugin.h>
 #include <stack_wallet_backup/stack_wallet_backup_plugin.h>
@@ -25,6 +26,9 @@ void fl_register_plugins(FlPluginRegistry* registry) {
   g_autoptr(FlPluginRegistrar) flutter_libmonero_registrar =
       fl_plugin_registry_get_registrar_for_plugin(registry, "FlutterLibmoneroPlugin");
   flutter_libmonero_plugin_register_with_registrar(flutter_libmonero_registrar);
+  g_autoptr(FlPluginRegistrar) flutter_libwownero_registrar =
+      fl_plugin_registry_get_registrar_for_plugin(registry, "FlutterLibmoneroPlugin");
+  flutter_libmonero_plugin_register_with_registrar(flutter_libwownero_registrar);
   g_autoptr(FlPluginRegistrar) flutter_secure_storage_linux_registrar =
       fl_plugin_registry_get_registrar_for_plugin(registry, "FlutterSecureStorageLinuxPlugin");
   flutter_secure_storage_linux_plugin_register_with_registrar(flutter_secure_storage_linux_registrar);
