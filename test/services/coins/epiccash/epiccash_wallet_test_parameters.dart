edeaa29304c73c9ccb2c8b332c6d2d8c0d7c189c
sneurlax <sneurlax@gmail.com>
2023-02-09 15:11:05 -0600

    comment update

--- test/services/coins/epiccash/epiccash_wallet_test_parameters.dart
+++ test/services/coins/epiccash/epiccash_wallet_test_parameters.dart
@@ -1,4 +1,7 @@
-// use `git update-index --skip-worktree epiccash_wallet_test_parameters.dart` to ignore changes to this file if you make changes
+// to ignore changes to this file if you make changes:
+// use `git update-index --skip-worktree epiccash_wallet_test_parameters.dart`
+// do not commit mnemonic, WIF, etc. ... to undo and commit changes, use
+// use `git update-index --no-skip-worktree epiccash_wallet_test_parameters.dart`
 
 const TEST_MNEMONIC = "";
 const ROOT_WIF = "";
