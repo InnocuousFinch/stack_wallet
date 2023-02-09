19402ea68070ff89166a800fd065145ae0048c0a
sneurlax <sneurlax@gmail.com>
2023-02-09 15:07:00 -0600

    add and ignore stub test parameters file

---
+++ test/services/coins/epiccash/epiccash_wallet_test_parameters.dart
@@ -0,0 +1,5 @@
+// use `git update-index --skip-worktree epiccash_wallet_test_parameters.dart` to ignore changes to this file if you make changes
+
+const TEST_MNEMONIC = "";
+const ROOT_WIF = "";
+const NODE_WIF_84 = "";
